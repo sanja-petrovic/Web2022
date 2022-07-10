e77ccc42791c1c3fd8ef2c6e71127de7b2e32dea
Sanya <sanjica.petrovic7@gmail.com>
2022-07-10 13:23:55 +0200

    Not found page

--- static/js/App.js
+++ static/js/App.js
@@ -20,6 +20,7 @@ const MembershipsAdmin = { template: '<memberships-admin></memberships-admin>' }
 const TrainingsAdmin = { template: '<trainings-admin></trainings-admin>' }
 const ContentsAdmin = { template: '<contents-admin></contents-admin>'}
 const UnauthorizedAccess = { template: '<unauthorized-access></unauthorized-access>'}
+const NotFound = { template: '<not-found></not-found>'}
 
 
 const router = new VueRouter({
@@ -43,7 +44,8 @@ const router = new VueRouter({
         { path: '/dodaj-promokod', component: PromoCode },
         { path: '/obrisi-clanarinu', component: MembershipsAdmin },
         { path: '/obrisi-trening', component: TrainingsAdmin },
-        { path: '/obrisi-sadrzaj', component: ContentsAdmin }
+        { path: '/obrisi-sadrzaj', component: ContentsAdmin },
+        { path: '*', component: NotFound }
         
       
     ]
